f9c82fa0757aea4945493d07f927550d72aa4a98
Ravi Shankar <wafflespeanut@gmail.com>
2015-09-22 16:42:36 +0530

    meta items and initial version of pretty outputs!

--- src/sorter.rs
+++ src/sorter.rs
@@ -1,11 +1,10 @@
 use rustc::lint::{Context, LintPass, LintArray};
 use std::cmp::Ordering;
-use std::fmt::Display;
-use syntax::ast::{Mod, Item_, PathListItem_, ViewPath_};
-use syntax::codemap::{Pos, BytePos, Span};
+use syntax::ast::{Mod, Item, Item_, Lit_, PathListItem_, ViewPath_, Visibility, MetaItem_};
+use syntax::codemap::Span;
 use syntax::print::pprust::path_to_string;
 
-declare_lint!(UNSORTED_DECLARATIONS, Deny,
+declare_lint!(UNSORTED_DECLARATIONS, Warn,
               "Warn when the declarations of crates or modules are not in alphabetical order");
 
 pub struct Sorter;
@@ -21,23 +20,26 @@ impl LintPass for Sorter {
         let mut uses = Vec::new();
         let mut mods = Vec::new();
         for item in &module.items {
-            let item_name = item.ident.name.as_str();
+            let item_name = format!("{}", item.ident.name.as_str());
             let item_span = item.span;
             match item.node.clone() {
                 Item_::ItemExternCrate(_) if item_name != "std" => {
-                    extern_crates.push((item_name, item_span));
+                    let item_attrs = get_item_attrs(&item, false);
+                    extern_crates.push((item_name, item_attrs, item_span, false));
                 },
                 Item_::ItemMod(module) => {
                     let mod_invoked_file = session_codemap.span_to_filename(item.span);
                     let mod_declared_file = session_codemap.span_to_filename(module.inner);
-                    if mod_declared_file != mod_invoked_file {      // this ignores inline modules
-                        mods.push((item_name, item_span));
+                    if mod_declared_file != mod_invoked_file {      // ignores inline modules
+                        let item_attrs = get_item_attrs(&item, true);
+                        mods.push((item_name, item_attrs, item_span, false));
                     }
                 },
                 Item_::ItemUse(spanned) => {
+                    let item_attrs = get_item_attrs(&item, true);
                     match spanned.node {
                         ViewPath_::ViewPathSimple(_, ref path) => {
-                            uses.push((path_to_string(&path), item_span));
+                            uses.push((path_to_string(&path), item_attrs, item_span, false));
                         },
                         ViewPath_::ViewPathList(ref path, ref list) => {
                             let old_list = list
@@ -59,22 +61,22 @@ impl LintPass for Sorter {
                                     false => a.cmp(b),
                                 }
                             });
+                            let mut warn = false;
                             for i in 0..old_list.len() {
                                 if old_list[i] != new_list[i] {
-                                    let suggestion = format!("use lists should be in alphabetical order!\
-                                                              \n\texpected order of list: {:?}",
-                                                              new_list);
-                                    cx.span_lint(UNSORTED_DECLARATIONS, path.span, &suggestion);
+                                    warn = true;
                                     break;
                                 }
-                            }
+                            }   // the `connect` methods should be replaced with `join` ASAP!
+                            let use_list = format!("{}::{{{}}}", path_to_string(&path), new_list.connect(", "));
+                            uses.push((use_list, item_attrs, path.span, warn));
                         },
                         ViewPath_::ViewPathGlob(ref path) => {
                             let path_str = path_to_string(&path);
                             // we don't have any use statements like `use std::prelude::*`
                             // since it's done only by rustc, we can safely neglect those here
                             if !path_str.starts_with("std::") {
-                                uses.push((path_str, item_span));
+                                uses.push((path_str, item_attrs, item_span, false));
                             }
                         },
                     }
@@ -87,24 +89,83 @@ impl LintPass for Sorter {
         check_sort(&mods, cx, "module declarations", "mod");
         check_sort(&uses, cx, "use statements", "use");
 
-        fn check_sort<T: Ord + PartialEq + Clone + Display>(old_slice: &[(T, Span)],
-                                                            cx: &Context,
-                                                            kind: &str,
-                                                            syntax: &str) {
-            let mut new_slice = old_slice
+        fn get_item_attrs(item: &Item, pub_check: bool) -> String {
+            let attr_vec = item.attrs
+                           .iter()
+                           .map(|attr| {
+                                let meta_item = attr.node.value.node.clone();
+                                format!("#[{}]", get_meta_as_string(&meta_item))
+                           }).collect::<Vec<String>>();
+            let attr_string = attr_vec.connect("\n");
+            match item.vis {
+                Visibility::Public if pub_check => {
+                    match attr_string.is_empty() {
+                        true => "pub ".to_owned(),
+                        false => attr_string + "\npub ",
+                    }
+                },
+                _ => {
+                    match attr_string.is_empty() {
+                        true => attr_string,
+                        false => attr_string + "\n",
+                    }
+                },
+            }
+        }
+
+        fn get_meta_as_string(meta_item: &MetaItem_) -> String {
+            match *meta_item {
+                MetaItem_::MetaWord(ref string) => format!("{}", string),
+                MetaItem_::MetaList(ref string, ref meta_items) => {
+                    let stuff = meta_items
                                 .iter()
-                                .map(|&(ref string, _span)| string.clone())
-                                .collect::<Vec<T>>();
-            new_slice.sort();
-            for i in 0..old_slice.len() {
-                let (name, mut span) = (old_slice[i].0.clone(), old_slice[i].1);
-                span.lo = span.lo + BytePos::from_usize(syntax.len() + 1);
-                if name != new_slice[i] {
-                    let suggestion = format!("{} should be in alphabetical order!\n\t\
-                                              expected: \"{} {};\"",
-                                              kind, syntax, new_slice[i]);
-                    cx.span_lint(UNSORTED_DECLARATIONS, span, &suggestion);
+                                .map(|meta_item| {
+                                    get_meta_as_string(&meta_item.node)
+                                }).collect::<Vec<String>>();
+                    format!("{}({})", string, stuff.connect(", "))
+                },
+                MetaItem_::MetaNameValue(ref string, ref literal) => {
+                    let value = match literal.node {
+                        Lit_::LitStr(ref inner_str, _style) => inner_str,
+                        _ => panic!("unexpected literal found for meta item!"),
+                    };
+                    format!("{} = \"{}\"", string, value)
+                },
+            }
+        }
+
+        fn check_sort(old_list: &[(String, String, Span, bool)], cx: &Context, kind: &str, syntax: &str) {
+            let length = old_list.len();
+            let mut new_list = old_list
+                                .iter()
+                                .map(|&(ref name, ref attrs, _span, warn)| (name.clone(), attrs.clone(), warn))
+                                .collect::<Vec<(String, String, bool)>>();
+            new_list.sort_by(|&(ref str_a, _, _), &(ref str_b, _, _)| str_a.cmp(str_b));
+
+            let mut span: Option<Span> = None;
+            let message = format!("{} should be in alphabetical order!\nTry this...\n", kind);
+            let mut suggested_list = Vec::new();
+            suggested_list.push(message);
+
+            for i in 0..length {
+                let name = new_list[i].0.clone();
+                if (old_list[i].0 != name) || new_list[i].2 {
+                    let suggestion = format!("{}{} {};", new_list[i].1, syntax, name);
+                    suggested_list.push(suggestion);
+                    span = match span {
+                        Some(old_span) => {
+                            let mut sp = old_span;
+                            sp.hi = old_list[i].2.hi;
+                            Some(sp)
+                        },
+                        None => Some(old_list[i].2),
+                    }
                 }
+            }
+
+            match span {
+                Some(sp) => cx.span_lint(UNSORTED_DECLARATIONS, sp, &suggested_list.connect("\n")),
+                None => (),
             }
         }
     }
