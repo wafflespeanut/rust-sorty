b7df32b9fcca6465542dd03da8bacc93675dfb10
Ravi Shankar <wafflespeanut@gmail.com>
2015-09-22 20:34:21 +0530

    pattern matching and revised methods

--- src/sorter.rs
+++ src/sorter.rs
@@ -38,27 +38,35 @@ impl LintPass for Sorter {
                 Item_::ItemUse(spanned) => {
                     let item_attrs = get_item_attrs(&item, true);
                     match spanned.node {
-                        ViewPath_::ViewPathSimple(_, ref path) => {
-                            uses.push((path_to_string(&path), item_attrs, item_span, false));
+                        ViewPath_::ViewPathSimple(ref ident, ref path) => {
+                            let path_str = path_to_string(&path);
+                            let name = ident.name.as_str();
+                            let renamed = match path_str.ends_with(&*name) {
+                                true => path_str,   // for checking `use foo as bar`
+                                false => format!("{} as {}", &path_str, &name),
+                            };
+                            uses.push((renamed, item_attrs, item_span, false));
                         },
                         ViewPath_::ViewPathList(ref path, ref list) => {
                             let old_list = list
                                            .iter()
-                                           .filter_map(|&list_item| {
+                                           .map(|&list_item| {
                                                 match list_item.node {
+                                                    PathListItem_::PathListMod { .. } =>
+                                                        "self".to_owned(),
                                                     PathListItem_::PathListIdent { name, .. } => {
                                                         let interned = name.name.as_str();
                                                         let string = &*interned;
-                                                        Some(string.to_owned())
+                                                        string.to_owned()
                                                     },
-                                                    _ => None,
                                                 }
                                             }).collect::<Vec<String>>();
                             let mut new_list = old_list.clone();
                             new_list.sort_by(|a, b| {
-                                match *a == "self" {    // `self` should be first in an use list
-                                    true => Ordering::Less,
-                                    false => a.cmp(b),
+                                match (&**a, &**b) {    // `self` should be first in an use list
+                                    ("self", _) => Ordering::Less,
+                                    (_, "self") => Ordering::Greater,
+                                    _ => a.cmp(b),
                                 }
                             });
                             let mut warn = false;
@@ -72,7 +80,7 @@ impl LintPass for Sorter {
                             uses.push((use_list, item_attrs, path.span, warn));
                         },
                         ViewPath_::ViewPathGlob(ref path) => {
-                            let path_str = path_to_string(&path);
+                            let path_str = path_to_string(&path) + "::*";
                             // we don't have any use statements like `use std::prelude::*`
                             // since it's done only by rustc, we can safely neglect those here
                             if !path_str.starts_with("std::") {
@@ -86,9 +94,10 @@ impl LintPass for Sorter {
         }
 
         check_sort(&extern_crates, cx, "crate declarations", "extern crate");
-        check_sort(&mods, cx, "module declarations", "mod");
+        check_sort(&mods, cx, "module declarations (ignoring the inline modules)", "mod");
         check_sort(&uses, cx, "use statements", "use");
 
+        // for collecting, formatting & filtering the meta items, and checking the visibility
         fn get_item_attrs(item: &Item, pub_check: bool) -> String {
             let attr_vec = item.attrs
                            .iter()
@@ -117,8 +126,9 @@ impl LintPass for Sorter {
             }
         }
 
+        // recursively collect the information from meta items into Strings
         fn get_meta_as_string(meta_item: &MetaItem_) -> String {
-            match *meta_item {      // recursively collect the information from meta items into Strings
+            match *meta_item {
                 MetaItem_::MetaWord(ref string) => format!("{}", string),
                 MetaItem_::MetaList(ref string, ref meta_items) => {
                     let stuff = meta_items
@@ -137,7 +147,8 @@ impl LintPass for Sorter {
             }
         }
 
-        // slice of tuples with a name, related attributes, spans and whether to warn for an use list
+        // checks the sorting of all the declarations and raises warnings whenever necessary
+        // takes a slice of tuples with a name, related attributes, spans and whether to warn for an unordered use list
         fn check_sort(old_list: &[(String, String, Span, bool)], cx: &Context, kind: &str, syntax: &str) {
             let length = old_list.len();
             let mut new_list = old_list
@@ -145,43 +156,42 @@ impl LintPass for Sorter {
                                 .map(|&(ref name, ref attrs, _span, warn)| (name.clone(), attrs.clone(), warn))
                                 .collect::<Vec<(String, String, bool)>>();
             new_list.sort_by(|&(ref str_a, ref attr_a, _), &(ref str_b, ref attr_b, _)| {
-                match attr_b.ends_with("pub ") {
-                    true => {   // a closure only to move the ordered `pub` statements to the bottom
-                        let new_str_b = "~".to_owned() + &str_b;
-                        match attr_a.ends_with("pub ") {
-                            true => {   // since `~` is superior to almost all the ASCII chars
-                                let new_str_a = "~".to_owned() + &str_a;
-                                new_str_a.cmp(&new_str_b)
-                            },
-                            false => str_a.cmp(&new_str_b)
-                        }
-                    },
-                    false => str_a.cmp(str_b),
+                // a closure only to move the ordered `pub` statements to the bottom
+                let new_str_a = "~".to_owned() + &str_a;    // since `~` is superior to almost all the ASCII chars
+                let new_str_b = "~".to_owned() + &str_b;
+                match (attr_a.ends_with("pub "), attr_b.ends_with("pub ")) {
+                    (true, true) => new_str_a.cmp(&new_str_b),
+                    (true, false) => new_str_a.cmp(str_b),
+                    (false, true) => str_a.cmp(&new_str_b),
+                    (false, false) => str_a.cmp(str_b),
                 }
             });
 
+            let mut index = 0;
             let mut span: Option<Span> = None;
-            let message = format!("{} should be in alphabetical order!\nTry this...\n", kind);
-            let mut suggested_list = Vec::new();
-            suggested_list.push(message);
             for i in 0..length {
-                let name = new_list[i].0.clone();
-                if (old_list[i].0 != name) || new_list[i].2 {
-                    let suggestion = format!("{}{} {};", new_list[i].1, syntax, name);
-                    suggested_list.push(suggestion);
-                    span = match span {
-                        Some(old_span) => {
-                            let mut sp = old_span;
-                            sp.hi = old_list[i].2.hi;   // increase the span to include more lines
-                            Some(sp)
-                        },
-                        None => Some(old_list[i].2),
-                    }
+                if (old_list[i].0 != new_list[i].0) || new_list[i].2 {
+                    span = Some(old_list[i].2);
+                    index = i;      // only to find the index of the first unsorted declaration
+                    break;
                 }
             }
 
             match span {
-                Some(sp) => cx.span_lint(UNSORTED_DECLARATIONS, sp, &suggested_list.connect("\n")),
+                Some(span_start) => {   // print all the declarations proceeding the first unsorted one
+                    let suggestion_list = (index..length)
+                                          .map(|i| {
+                                              if i == length - 1 {  // increase the span to include more lines
+                                                  let mut sp = span_start;
+                                                  sp.hi = old_list[i].2.hi;
+                                                  span = Some(sp);
+                                              } format!("{}{} {};", new_list[i].1, syntax, new_list[i].0)
+                                          }).collect::<Vec<String>>();
+                    let suggestion = format!("{} should be in alphabetical order!\nTry this...\n\n{}\n\n",
+                                            kind, suggestion_list.connect("\n"));
+                    // unwrapping the value here, because it's certain there's something in `span`
+                    cx.span_lint(UNSORTED_DECLARATIONS, span.unwrap(), &suggestion);
+                },
                 None => (),
             }
         }
