b17f663ba27b6123033264ddc50bcbc8d12671fc
Ravi Shankar <wafflespeanut@gmail.com>
2016-06-05 21:20:44 +0530

    Minor cleanup

--- src/sorty.rs
+++ src/sorty.rs
@@ -1,13 +1,12 @@
 use rustc::lint::{EarlyContext, EarlyLintPass, LintArray, LintContext, LintPass};
 use std::cmp::Ordering;
-use syntax::ast::{Item, ItemKind, LitKind, MetaItemKind, Mod, PathListItemKind, ViewPath_,
-                  Visibility};
+use syntax::ast::{Item, ItemKind, LitKind, MetaItemKind, Mod, PathListItemKind};
+use syntax::ast::{ViewPath_, Visibility};
 use syntax::codemap::Span;
 use syntax::print::pprust::path_to_string;
 
 // Warn unsorted declarations by default (since denying is a poor choice for styling lints)
-declare_lint!(UNSORTED_DECLARATIONS,
-              Warn,
+declare_lint!(UNSORTED_DECLARATIONS, Warn,
               "Warn when the declarations of crates or modules are not in alphabetical order");
 
 pub struct Sorty;
@@ -43,6 +42,7 @@ impl EarlyLintPass for Sorty {
                         Some(old_name) => format!("{}extern crate {} as", item_attrs, old_name),
                         None => format!("{}extern crate", item_attrs),
                     };
+
                     extern_crates.push((item_name, item_attrs, item_span, false));
                 }
 
@@ -63,7 +63,7 @@ impl EarlyLintPass for Sorty {
                             let path_str = path_to_string(&path);
                             let name = ident.name.as_str();
                             let renamed = {
-                                // for `use foo as bar`
+                                // `use foo as bar`
                                 let split = path_str.split(":").collect::<Vec<&str>>();
                                 match split[split.len() - 1] == &*name {
                                     true => path_str.clone(),
@@ -81,19 +81,18 @@ impl EarlyLintPass for Sorty {
                                                        PathListItemKind::Mod { .. } => {
                                                            // this must be `self`
                                                            "self".to_owned()
-                                                       }
+                                                       },
                                                        PathListItemKind::Ident { name, .. } => {
                                                            let interned = name.name.as_str();
                                                            let string = &*interned;
                                                            string.to_owned()
-                                                       }
+                                                       },
                                                    }
-                                               })
-                                               .collect::<Vec<String>>();
+                                               }).collect::<Vec<_>>();
 
                             let mut new_list = old_list.clone();
                             new_list.sort_by(|a, b| {
-                                match (&**a, &**b) {    // `self` should be first in an use list
+                                match (&**a, &**b) {    // `self` should be first in a list of use items
                                     ("self", _) => Ordering::Less,
                                     (_, "self") => Ordering::Greater,
                                     _ => a.cmp(b),
@@ -110,6 +109,7 @@ impl EarlyLintPass for Sorty {
                                     break;
                                 }
                             }
+
                             uses.push((use_list, item_attrs, path.span, warn));
                         }
 
@@ -130,10 +130,7 @@ impl EarlyLintPass for Sorty {
         // we don't include the crate declaration here, because we've already appended it with the
         // attributes
         check_sort(&extern_crates, cx, "crate declarations", "");
-        check_sort(&mods,
-                   cx,
-                   "module declarations (other than inline modules)",
-                   "mod");
+        check_sort(&mods, cx, "module declarations (other than inline modules)", "mod");
         check_sort(&uses, cx, "use statements", "use");
 
         // for collecting, formatting & filtering the attributes (and checking the visibility)
@@ -147,8 +144,7 @@ impl EarlyLintPass for Sorty {
                                            true => None,
                                            false => Some(format!("#[{}]", meta_string)),
                                        }
-                                   })
-                                   .collect::<Vec<String>>();
+                                   }).collect::<Vec<_>>();
 
             attr_vec.sort_by(|a, b| {
                 match (&**a, &**b) {    // put `macro_use` first for later checking
@@ -157,21 +153,21 @@ impl EarlyLintPass for Sorty {
                     _ => a.cmp(b),
                 }
             });
-            let attr_string = attr_vec.join("\n");
 
+            let attr_string = attr_vec.join("\n");
             match item.vis {
                 Visibility::Public if pub_check => {
                     match attr_string.is_empty() {
                         true => "pub ".to_owned(),
                         false => attr_string + "\npub ",    // `pub` for mods and uses
                     }
-                }
+                },
                 _ => {
                     match attr_string.is_empty() {
                         true => attr_string,
                         false => attr_string + "\n",
                     }
-                }
+                },
             }
         }
 
@@ -182,53 +178,58 @@ impl EarlyLintPass for Sorty {
                 MetaItemKind::List(ref string, ref meta_items) => {
                     let stuff = meta_items.iter()
                                           .map(|meta_item| get_meta_as_string(&meta_item.node))
-                                          .collect::<Vec<String>>();
+                                          .collect::<Vec<_>>();
                     format!("{}({})", string, stuff.join(", "))
-                }
+                },
                 MetaItemKind::NameValue(ref string, ref literal) => {
                     let value = match literal.node {
                         LitKind::Str(ref inner_str, _style) => inner_str,
-                        _ => {
-                            // doesn't really happen
-                            panic!("unexpected literal found for meta item!")
-                        }
+                        // well, doesn't really happen
+                        _ => panic!("unexpected literal found for meta item!"),
+
                     };
+
                     format!("{} = \"{}\"", string, value)
-                }
+                },
             }
         }
 
         // checks the sorting of all the declarations and raises warnings whenever necessary
         // takes a slice of tuples with name, related attributes, spans and whether to warn for
         // unordered use lists
-        fn check_sort(old_list: &[(String, String, Span, bool)],
-                      cx: &EarlyContext,
-                      kind: &str,
-                      syntax: &str) {
+        fn check_sort(old_list: &[(String, String, Span, bool)], cx: &EarlyContext,
+                      kind: &str, syntax: &str) {
+
+            // prepend given characters to the names for "biased" sorting
+            fn str_for_biased_sort(string: &str, choice: bool, prepend_char: &str) -> String {
+                match choice {
+                    true => prepend_char.to_owned() + string,
+                    false => string.to_owned(),
+                }
+            }
+
             let length = old_list.len();
             let mut new_list = old_list.iter()
                                        .map(|&(ref name, ref attrs, _span, warn)| {
                                            (name.clone(), attrs.clone(), warn)
-                                       })
-                                       .collect::<Vec<(String, String, bool)>>();
+                                       }).collect::<Vec<(String, String, bool)>>();
+
             new_list.sort_by(|&(ref str_a, ref attr_a, _), &(ref str_b, ref attr_b, _)| {
-                // move the `pub` statements below (with `~` since it's on the farther side of
-                // ASCII)
+                // move the `pub` statements below
+                // (with `~` since it's on the farther side of ASCII)
                 let mut new_str_a = str_for_biased_sort(&str_a, attr_a.ends_with("pub "), "~");
                 let mut new_str_b = str_for_biased_sort(&str_b, attr_b.ends_with("pub "), "~");
-                // move the #[macro_use] stuff above (with `!` since it's on the lower extreme of
-                // ASCII)
+                // move the #[macro_use] stuff above
+                // (with `!` since it's on the lower extreme of ASCII)
                 new_str_a = str_for_biased_sort(&new_str_a,
-                                                attr_a.starts_with("#[macro_use]"),
-                                                "!");
+                                                attr_a.starts_with("#[macro_use]"), "!");
                 new_str_b = str_for_biased_sort(&new_str_b,
-                                                attr_b.starts_with("#[macro_use]"),
-                                                "!");
+                                                attr_b.starts_with("#[macro_use]"), "!");
                 new_str_a.cmp(&new_str_b)
             });
 
             let mut index = 0;
-            let mut span: Option<Span> = None;
+            let mut span = None;
             for i in 0..length {
                 if (old_list[i].0 != new_list[i].0) || new_list[i].2 {
                     span = Some(old_list[i].2);
@@ -239,38 +240,26 @@ impl EarlyLintPass for Sorty {
                 }
             }
 
-            match span {
-                Some(span_start) => {
-                    // print all the declarations proceeding the first unsorted one
-                    let suggestion_list = (index..length)
-                                              .map(|i| {
-                                                  if i == length - 1 {
-                                                      // increase the span to include more lines
-                                                      let mut sp = span_start;
-                                                      sp.hi = old_list[i].2.hi;
-                                                      span = Some(sp);
-                                                  }
-                                                  format!("{}{} {};",
-                                                          new_list[i].1,
-                                                          syntax,
-                                                          new_list[i].0)
-                                              })
-                                              .collect::<Vec<String>>();
-                    let message = format!("{} should be in alphabetical order!", kind);
-                    let suggestion = format!("Try this...\n\n{}\n", suggestion_list.join("\n"));
-                    // unwrapping the value here, because it's quite certain that there's something
-                    // in `span`
-                    cx.span_lint_help(UNSORTED_DECLARATIONS, span_start, &message, &suggestion);
-                }
-                None => (),
-            }
+            if let Some(span_start) = span {
+                // print all the declarations proceeding the first unsorted one
+                let suggestion_list = (index..length)
+                                          .map(|i| {
+                                              if i == length - 1 {
+                                                  // increase the span to include more lines
+                                                  let mut sp = span_start;
+                                                  sp.hi = old_list[i].2.hi;
+                                                  span = Some(sp);
+                                              }
 
-            // prepend given characters to the names for "biased" sorting
-            fn str_for_biased_sort(string: &String, choice: bool, prepend_char: &str) -> String {
-                match choice {
-                    true => prepend_char.to_owned() + &string,
-                    false => string.clone(),
-                }
+                                              format!("{}{} {};", new_list[i].1,
+                                                      syntax, new_list[i].0)
+                                          }).collect::<Vec<String>>();
+
+                let message = format!("{} should be in alphabetical order!", kind);
+                let suggestion = format!("Try this...\n\n{}\n", suggestion_list.join("\n"));
+                // unwrapping the value here, because it's quite certain that there's something
+                // in `span`
+                cx.span_lint_help(UNSORTED_DECLARATIONS, span_start, &message, &suggestion);
             }
         }
     }
